52728ea2dca6addd96025e33d20b99c0e474c036
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2024-09-04 11:13:56 +0200

    nav links added router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import logo from './logo.png'; 
 import './header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -17,10 +18,10 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav className="ml-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/gallery">Gallery</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Link className='nav-link' to="/">Home</Link>
+            <Link  className='nav-link'to="/about">About</Link>
+            <Link className='nav-link' to="/gallery">Gallery</Link>
+            <Link className='nav-link' to="/contact">Contact</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
